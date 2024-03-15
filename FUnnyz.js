3b4aa6144fd4720bab9cdb2b8d9530a323c37ae0
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-14 17:30:13 -0700

    Update FUnnyz.js

--- FUnnyz.js
+++ FUnnyz.js
@@ -1,11 +1,9 @@
-ModAPI.require("player")
+ModAPI.require("player");
 
 ModAPI.addEventListener("update", function () {
   var currentItem = ModAPI.player.inventory.currentItem;
   if (currentItem) {
-    setTimeout(function() {
-      var nbtData = JSON.stringify(currentItem.getNBT(), null, 2); // Parse and format NBT data
-      ModAPI.displayToChat({msg: "NBT Data of Current Item:\n" + nbtData});
-    }, 1000); // Delay for 1 second before displaying NBT data
+    var nbtData = ModAPI.getNBTData(currentItem); // Assuming this method exists in your custom API
+    ModAPI.displayToChat({msg: "NBT Data of Current Item: " + nbtData});
   }
-})
+});
