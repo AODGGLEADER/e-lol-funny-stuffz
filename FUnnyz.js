4e929e33ba420e0990617dfc133b11fdd51e7e07
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-14 17:23:53 -0700

    Update FUnnyz.js

--- FUnnyz.js
+++ FUnnyz.js
@@ -3,7 +3,9 @@ ModAPI.require("player")
 ModAPI.addEventListener("update", function () {
   var currentItem = ModAPI.player.inventory.currentItem;
   if (currentItem) {
-    var nbtData = currentItem.getNBT();
-    ModAPI.displayToChat({msg: nbtData});
+    setTimeout(function() {
+      var nbtData = JSON.stringify(currentItem.getNBT(), null, 2); // Parse and format NBT data
+      ModAPI.displayToChat({msg: "NBT Data of Current Item:\n" + nbtData});
+    }, 1000); // Delay for 1 second before displaying NBT data
   }
 })
