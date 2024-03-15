25bc29e9bb4701e9d0a4fd880bddc6c276d77e6f
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-03-14 20:00:36 -0700

    Update FUnnyz.js

--- FUnnyz.js
+++ FUnnyz.js
@@ -1,12 +1,4 @@
-// Assuming you have a custom API object named ModAPI
-// and it provides a method to get the NBT data of the current item
-
-ModAPI.require("player");
-
-ModAPI.addEventListener("update", function () {
-  var currentItem = ModAPI.player.inventory.currentItem;
-  if (currentItem) {
-    var nbtData = ModAPI.getNBTData(currentItem); // Assuming this method exists in your custom API
-    ModAPI.displayToChat("NBT Data of Current Item: " + nbtData);
-  }
-});
+ModAPI.require("player")
+ModAPI.addEventListener("update", function (){
+  ModAPI.displayToChat({msg: ModAPI.player.inventory.currentItem.toString()})
+})
