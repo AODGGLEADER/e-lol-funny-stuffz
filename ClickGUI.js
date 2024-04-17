10e68df125d85946a7fb3bedb41046baabccf7ef
(AOD)GG{LEADER} <114262675+AODGGLEADER@users.noreply.github.com>
2024-04-16 18:40:58 -0700

    Update ClickGUI.js

--- ClickGUI.js
+++ ClickGUI.js
@@ -1,20 +1,13 @@
-ModAPI.require('player');
 ModAPI.addEventListener("key", function(ev) {
     if (ev.key == 200) {
         ModAPI.displayToChat({
             msg: ModAPI.getProfileName()
         });
         ModAPI.displayToChat({
-            msg: (ModAPI.player ? ModAPI.player.X : "")
-        });
-        ModAPI.displayToChat({
-            msg: (ModAPI.player ? ModAPI.player.Y : "")
-        });
-        ModAPI.displayToChat({
-            msg: (ModAPI.player ? ModAPI.player.Z : "")
-        });
-        ModAPI.displayToChat({
             msg: ModAPI.getFPS()
+        });
+        ModAPI.displayToChat({
+            msg: you are gae hehehehaw
         });
     }
 });
